12bea56407fd59f23c215be73da3787b40accd10
olawale-o <omoogunolawale@gmail.com>
2023-02-02 13:21:56 +0100

    Generate doc for users route

--- src/users/index.js
+++ src/users/index.js
@@ -1,7 +1,52 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+/**
+* @swagger
+*   /users:
+*     post:
+*       summary: Create a new user
+*       tags: [User]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/UserSignUpRequest'
+*       responses:
+*         200:
+*           description: Task created successfully
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/User'
+*         500:
+*           description: Internal server error
+*/
 router.post('/', controller.new);
+
+/**
+* @swagger
+*   /users/login:
+*     post:
+*       summary: Login a user
+*       tags: [User]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/UserLoginRequest'
+*       responses:
+*         200:
+*           description: Task created successfully
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/User'
+*         500:
+*           description: Internal server error
+*/
 router.post('/login', controller.login);
 
 module.exports = router;
