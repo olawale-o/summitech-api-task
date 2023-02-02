ab1eabfe0d33fb879fa4574fbff1a5dfc336e7cd
olawale-o <omoogunolawale@gmail.com>
2023-02-02 13:22:23 +0100

    Generate doc for stocks route

--- src/stocks/index.js
+++ src/stocks/index.js
@@ -1,7 +1,48 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+/**
+* @swagger
+*   /stocks:
+*     post:
+*       summary: Create a new stock for a product
+*       tags: [Stock]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/NewStockRequest'
+*       responses:
+*         200:
+*           description: Product created successfully
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/Stock'
+*         500:
+*           description: Internal server error
+*/
 router.post('/', controller.newStock);
+
+/**
+* @swagger
+*   /stocks:
+*     get:
+*       summary: Get all products
+*       tags: [Stock]
+*       responses:
+*         200:
+*           description: Fetch all stocks successfully
+*           content:
+*             application/json:
+*               schema:
+*                 type: array
+*                 items:
+*                   $ref: '#/components/schemas/Stock'
+*         500:
+*           description: Internal server error
+*/
 router.get('/', controller.findAllStocks);
 
 module.exports = router;
