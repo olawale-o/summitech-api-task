39c6d17ab212544b656f634f46c686ca13d43d3c
olawale-o <omoogunolawale@gmail.com>
2023-02-02 13:22:08 +0100

    Generate doc for products route

--- src/products/index.js
+++ src/products/index.js
@@ -1,8 +1,73 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+/**
+* @swagger
+*   /products:
+*     post:
+*       summary: Create a new product
+*       tags: [Product]
+*       requestBody:
+*         required: true
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/NewProductRequest'
+*       responses:
+*         200:
+*           description: Product created successfully
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/Product'
+*         500:
+*           description: Internal server error
+*/
 router.post('/', controller.newProduct);
+
+/**
+* @swagger
+*   /products/{id}:
+*     get:
+*       summary: Get a product
+*       tags: [Product]
+*       parameters:
+*         - in: path
+*           name: id
+*           schema:
+*             type: integer
+*           required: true
+*           description: The product id
+*       responses:
+*         200:
+*           description: Fetch a product successfully
+*           content:
+*             application/json:
+*               schema:
+*                 $ref: '#/components/schemas/Product'
+*         500:
+*           description: Internal server error
+*/
 router.get('/:id', controller.findProduct);
+
+/**
+* @swagger
+*   /products:
+*     get:
+*       summary: Get all products
+*       tags: [Product]
+*       responses:
+*         200:
+*           description: Fetch all products successfully
+*           content:
+*             application/json:
+*               schema:
+*                 type: array
+*                 items:
+*                   $ref: '#/components/schemas/Product'
+*         500:
+*           description: Internal server error
+*/
 router.get('/', controller.findAllProducts);
 
 module.exports =  router;
