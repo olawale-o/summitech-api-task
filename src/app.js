7bbe2af8b7616a2d14920940b30e94ea9c1f20e6
olawale-o <omoogunolawale@gmail.com>
2023-02-02 13:23:02 +0100

    Display ui for swagger doc

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 //  const errorHandler = require('../middleware/errorHandler');
 
 const app = express();
+
+const swaggerOptions = require('../swagger-options');
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +20,7 @@ app.use('/api/v1/users', require('./users'));
 app.use('/api/v1/products', require('./products'));
 app.use('/api/v1/stocks', require('./stocks'));
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 // app.use(errorHandler);
 
 module.exports = app;
