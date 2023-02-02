7a43b53bafd1c181f56b391192943e088c2ca823
olawale-o <omoogunolawale@gmail.com>
2023-02-02 10:01:25 +0100

    Setup app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+//  const errorHandler = require('../middleware/errorHandler');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+// app.use('/api/v1/users', require('../routes/users'));
+// app.use('/api/v1/transactions', passport.authenticate('jwt', {session: false}), require('../routes/transactions'));
+
+// app.use(errorHandler);
+
+module.exports = app;
