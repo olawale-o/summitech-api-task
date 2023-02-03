76f6446495266027f42d37ae19e973e124638f77
olawale-o <omoogunolawale@gmail.com>
2023-02-03 05:38:03 +0100

    Validate product owner with user id

--- src/stocks/service.js
+++ src/stocks/service.js
@@ -1,11 +1,19 @@
 const repository = require('./repository');
 const userRepository = require('../users/repository');
+const producRepository = require('../products/repository');
 
 module.exports = {
   newStock: async (credentials) => {
     const user = await userRepository.findById({ id: credentials.userId });
     if (!user) {
       throw new Error('User not found');
+    }
+    const product = await producRepository.findById({ id: credentials.product_id });
+    if (!product) {
+      throw new Error('You are adding stock for product that do no exist');
+    }
+    if (product.user_id !== credentials.userId) {
+      throw new Error('This is not your product');
     }
     const data = await repository.insertOne({
       details: credentials.details,
