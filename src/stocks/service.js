4bd485a4111d01e0c2d8b8e7244c7cf891be5f4e
olawale-o <omoogunolawale@gmail.com>
2023-02-03 05:18:50 +0100

    Throw error if user does not exist before adding stock

--- src/stocks/service.js
+++ src/stocks/service.js
@@ -1,11 +1,17 @@
 const repository = require('./repository');
-const { v4: uuidv4 } = require('uuid');
+const userRepository = require('../users/repository');
 
 module.exports = {
   newStock: async (credentials) => {
+    const user = await userRepository.findById({ id: credentials.userId });
+    if (!user) {
+      throw new Error('User not found');
+    }
     const data = await repository.insertOne({
-      ...credentials,
-      batch_id: uuidv4(),
+      details: credentials.details,
+      qty: credentials.qty,
+      product_id: credentials.product_id,
+      batch_id: credentials.batch_id,
     });
     return data;
   },
